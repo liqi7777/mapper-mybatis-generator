f9053e58ff1f4761abd039a8c1cad9ac11a22e34
sky李齐 <sky.li@ixiaoshuidi.com>
2019-05-23 10:59:31 +0800

    mybatis 通用 mapper 自动生成

---
+++ src/main/java/com/liqi/mappermybatisgenerator/MapperMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.liqi.mappermybatisgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperMybatisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperMybatisGeneratorApplication.class, args);
+	}
+
+}
